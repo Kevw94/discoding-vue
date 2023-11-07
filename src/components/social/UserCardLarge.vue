024e9d4459dddd02944bc70f9593da24ecc7e989
wensa <traumtanzer@tutanota.com>
2023-11-07 15:17:42 +0100

    social view is done v2

--- src/components/social/UserCardLarge.vue
+++ src/components/social/UserCardLarge.vue
@@ -1,21 +1,21 @@
 <template>
-    <div class="divider w-full h-[0.5px] bg-white"></div>
-    <div class="userCardLarge w-full flex justify-between items-center hover:bg-gray-600 p-2">
-      <UserCard />
-      <div class="userCardButtons flex gap-2">
-        <button class="w-8 h-8 bg-gray-800 rounded-full flex items-center justify-center">
+  <div class="userCardLarge relative flex flex-col items-center w-full hover:bg-gray-600 p-2 rounded-lg">
+    <div class="divider absolute top-0 w-[98%] h-[1px] bg-gray-600"></div>
+    <div class="userCard flex w-full justify-between items-center">
+      <UserCard :hoverEffect="false" />
+      <div class="userCardButtons flex gap-2 items-center">
+        <button class="w-10 h-10 bg-gray-800 rounded-full flex items-center justify-center">
           <IconCall />
         </button>
-        <button class="w-8 h-8 bg-gray-800 rounded-full"></button>
+        <button class="w-10 h-10 bg-gray-800 rounded-full flex items-center justify-center">
+          <!-- Suppose you will put an icon here -->
+        </button>
       </div>
     </div>
-  </template>
-  
-  <script setup lang="ts">
+  </div>
+</template>
+
+<script setup lang="ts">
   import UserCard from './UserCard.vue';
   import IconCall from '../icons/IconCall.vue';
-  </script>
-  
-
-
-
+</script>
--- src/views/SocialView.vue
+++ src/views/SocialView.vue
@@ -3,8 +3,9 @@
         <div class="socialTopNav w-full h-10 bg-red flex bg-gray-600">
             <div class="searchConvo w-[15%] h-full bg-gray-800"></div>
             <div class="socialTabs flex flex-grow items-center">
+                <IconPerson class="ml-6" />
                 <span class="friends border-r border-white mr-6 pr-6 pl-2">Friends</span>
-                <SocialTabs />
+                <SocialTabs @update:tab="handleTabSelection" />
             </div>
             <div class="socialActions w-[30%]"></div>
         </div>
@@ -18,23 +19,56 @@
             <div class="socialCore flex-grow p-5 bg-gray-700 flex flex-col">
 
                 <SearchBar placeholder="Rechercher..." @search-update="handleSearch" />
-                <div class="friendsList w-full mt-6">
+                <span class="mt-5 mb-5">{{ currentTab }} - {{ tabCount }}</span>
+                <div class="friendsList w-full h-full overflow-y-scroll">
+                    <UserCardLarge />
+                    <UserCardLarge />
+                    <UserCardLarge />
+                    <UserCardLarge />
+                    <UserCardLarge />
+                    <UserCardLarge />
+                    <UserCardLarge />
+                    <UserCardLarge />
+                    <UserCardLarge />
+                    <UserCardLarge />
                     <UserCardLarge />
                 </div>
             </div>
-            <div class="socialActivities bg-gray-600 h-full w-[30%] border-l border-white"></div>
+            <div class="socialActivities bg-gray-700 h-full w-[30%] border-l border-gray-600"></div>
         </div>
     </section>
 </template>
 
-<script lang="ts" setup>
-import UserCard from '@/components/social/UserCard.vue'
-import SocialTabs from '@/components/social/SocialTabs.vue';
+<script setup lang="ts">
+import IconPerson from '../components/icons/IconPerson.vue'
+import UserCard from '@/components/social/UserCard.vue';
 import UserCardLarge from '@/components/social/UserCardLarge.vue';
-import SearchBar from '@/components/utils/SearchBar.vue'
+import { ref } from 'vue';
+import SocialTabs from '@/components/social/SocialTabs.vue';
+import SearchBar from '@/components/utils/SearchBar.vue';
 
+const currentTab = ref('All');
+const tabCount = ref(0); // This will be dynamic based on the tab
+
+function handleTabSelection(selectedTab: string) {
+  currentTab.value = selectedTab;
+  // Update tabCount here based on the selected tab
+  // This will be replaced with actual data fetching logic
+  tabCount.value = getTabCount(selectedTab);
+}
 
 function handleSearch(query: string) {
-      console.log('Search query:', query);
-    }
+  // Handle the search logic
+}
+
+function getTabCount(tab: string): number {
+  // Placeholder for actual data fetching logic
+  const counts = {
+    All: 42,
+    Online: 12,
+    Pending: 5,
+    Blocked: 2,
+  };
+  return counts[tab];
+}
 </script>
