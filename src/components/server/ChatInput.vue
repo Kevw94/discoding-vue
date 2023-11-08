af39a6c14959c47f54a71d9661133144e5c20bcf
Héloïse Le Lez <lelez.heloise@gmail.com>
2023-11-08 15:35:42 +0100

    Add chat

---
+++ src/components/server/ChatInput.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div class="grow-0 shrink-0 basis-8 px-3 pb-3 flex">
+		<input
+			class="bg-gray-600 rounded-md h-10 p-1.5 grow mx-1"
+			placeholder='Send a message in "chan name"'
+		/>
+		<a
+			class="self-center bg-gray-800 rounded-full h-10 grow-0 shrink-0 basis-10 text-center p-4 mx-1"
+		>
+		</a>
+	</div>
+</template>
+
+<style scoped></style>
