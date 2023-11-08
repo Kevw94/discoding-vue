c9b8e6453a80d4045197b23cfd7f6ff657ea0ae1
Héloïse Le Lez <lelez.heloise@gmail.com>
2023-11-08 15:26:32 +0100

    Add server list component (side bar)

---
+++ src/components/server/ServersList.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+const servers: string[] = [
+	'https://images.unsplash.com/photo-1611515966587-bcf02a82a5d9?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	'https://images.unsplash.com/photo-1596981643462-a7b1c1db616b?q=80&w=2730&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	'https://images.unsplash.com/photo-1574666831601-06c4865f7bc2?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	'https://images.unsplash.com/photo-1603826567467-5db06a76894d?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzh8fGR1Y2t8ZW58MHx8MHx8fDI%3D',
+	'https://images.unsplash.com/photo-1611515966587-bcf02a82a5d9?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	'https://images.unsplash.com/photo-1596981643462-a7b1c1db616b?q=80&w=2730&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	'https://images.unsplash.com/photo-1574666831601-06c4865f7bc2?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	'https://images.unsplash.com/photo-1603826567467-5db06a76894d?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzh8fGR1Y2t8ZW58MHx8MHx8fDI%3D',
+	'https://images.unsplash.com/photo-1611515966587-bcf02a82a5d9?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	'https://images.unsplash.com/photo-1596981643462-a7b1c1db616b?q=80&w=2730&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	'https://images.unsplash.com/photo-1574666831601-06c4865f7bc2?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	'https://images.unsplash.com/photo-1603826567467-5db06a76894d?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzh8fGR1Y2t8ZW58MHx8MHx8fDI%3D',
+	'https://images.unsplash.com/photo-1611515966587-bcf02a82a5d9?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	'https://images.unsplash.com/photo-1596981643462-a7b1c1db616b?q=80&w=2730&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	'https://images.unsplash.com/photo-1574666831601-06c4865f7bc2?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	'https://images.unsplash.com/photo-1603826567467-5db06a76894d?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzh8fGR1Y2t8ZW58MHx8MHx8fDI%3D'
+]
+</script>
+
+<template>
+	<div>
+		<div class="grow">
+			<ul>
+				<li v-for="server in servers">
+					<div class="h-16 w-16 p-2">
+						<a href="">
+							<img
+								class="h-full w-full object-cover rounded-[50px] transition-all duration-300 delay-0 ease-in-out hover:rounded-[15px]"
+								:src="server"
+								alt="Server profile pic"
+							/>
+						</a>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<style scoped></style>
